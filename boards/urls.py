423cc395d02d797aa67892eff4d637dba0ec18f7
= <=>
2018-03-20 06:37:41 +0100

    Przed skończeniem maili z reset haseł

--- boards/urls.py
+++ boards/urls.py
@@ -6,4 +6,5 @@ app_name = 'boards'
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Board
+from .models import Board, User, Topic, Post
+from .forms import NewTopicForm
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
@@ -14,3 +15,26 @@ def home(request):
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'boards/topics.html', {'board': board})
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()  # TODO: get the currently logged in user
+    if request.method == "POST":
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+        # TODO: redirect to the created topic
+            return redirect('boards:board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
+
+    return render(request, 'boards/new_topic.html', {'board': board, 'form': form})
